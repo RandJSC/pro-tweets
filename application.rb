84c6eef035bdf9d09336f68592b34b16bff50e49
Mike Green <mike.is.green@gmail.com>
2012-02-06 23:05:37 -0500

    expandtab|retab

--- application.rb
+++ application.rb
@@ -6,26 +6,26 @@ require "yaml"
 
 class ProTweets < Sinatra::Application
 
-	configure do
-		Config = Hashie::Mash.new(YAML.load_file(File.join(File.dirname(__FILE__), 'config.yml')))
+  configure do
+    Config = Hashie::Mash.new(YAML.load_file(File.join(File.dirname(__FILE__), 'config.yml')))
 
-		Twitter.configure do |conf|
-			conf.consumer_key       = Config.twitter.consumer_key
-			conf.consumer_secret    = Config.twitter.consumer_secret
-			conf.oauth_token        = Config.twitter.oauth_token
-			conf.oauth_token_secret = Config.twitter.oauth_token_secret
-		end
-	end
+    Twitter.configure do |conf|
+      conf.consumer_key       = Config.twitter.consumer_key
+      conf.consumer_secret    = Config.twitter.consumer_secret
+      conf.oauth_token        = Config.twitter.oauth_token
+      conf.oauth_token_secret = Config.twitter.oauth_token_secret
+    end
+  end
 
-	get "/" do
-		content_type 'text/plain'
-		@tips = Twitter.search("#protip")
-		@tips.shuffle.first.attrs['text']
-	end
+  get "/" do
+    content_type 'text/plain'
+    @tips = Twitter.search("#protip")
+    @tips.shuffle.first.attrs['text']
+  end
 
-	get "/ping" do
-		"PONG"
-	end
+  get "/ping" do
+    "PONG"
+  end
 end
 
-# vim: set ft=ruby ts=2 sw=2 :
+# vim: set ft=ruby ts=2 sw=2 expandtab :
