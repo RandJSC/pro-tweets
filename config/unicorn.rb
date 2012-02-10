00e6d6bd6bd09599bde8cf0a2145e60380b1a51e
Mike Green <mike.is.green@gmail.com>
2012-02-09 23:23:53 -0500

    Added stdout and stderr log paths to unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,8 +1,16 @@
 # Unicorn Configuration File
 
 pid File.join(File.dirname(__FILE__), '..', 'tmp', 'unicorn.pid')
+
+# Logging
+log_path = File.join(File.dirname(__FILE__), '..', 'log')
+stderr_path File.join(log_path, 'unicorn.stderr.log')
+stdout_path File.join(log_path, 'unicorn.stdout.log')
+
+# Number of workers
 worker_processes 2
 
+# Listen on UNIX socket
 listen "/tmp/pro_tweets.sock", :backlog => 64
 
 after_fork do |server,worker|
